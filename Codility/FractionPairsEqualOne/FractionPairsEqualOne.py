cfc51d30466c9c01555bc2364853d355b283118a
serhattural <serhattural12@gmail.com>
2020-11-23 09:28:47 +0300

    Create FractionPairsEqualOne.py

---
+++ Codility/FractionPairsEqualOne/FractionPairsEqualOne.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    '''def countPairsEqOne(x, y):''' 
+    def gcd(self, a, b):
+        if b == 0:
+            return a
+        else:
+            return self.gcd(b, (a % b))
+        
+    def countPairsEqOne(self, X, Y):
+        maxCountValue = (pow(10,9) + 7)
+        mapFr = {}
+        n = len(X)
+        count = 0
+        for i in range(n):
+            xi = X[i]
+            yi = Y[i]
+            gcdXY = self.gcd(xi, yi)
+            xi = xi/gcdXY
+            yi = yi/gcdXY
+            key = "{}/{}".format(xi, yi)
+            pairkey = "{}/{}".format(yi-xi, yi)
+            if pairkey in mapFr:
+                count += mapFr[pairkey]
+                count = count % maxCountValue
+                
+            mapFr[key] = mapFr.get(key, 0) + 1
+        
+        return count
+
+s = Solution()
+X = [1,1,2];
+Y = [3,2,3];
+print("test1: ", s.countPairsEqOne(X, Y))
+X = [1,1,1];
+Y = [2,2,2];
+print("test2: ", s.countPairsEqOne(X, Y))
